66d4f09488bc14e2bc6ad523d4d308b6855447a5
Mikol Graves <mikol@thinbox.org>
2016-03-12 20:33:04 -0800

    Refine example.js.

--- example.js
+++ example.js
@@ -7,29 +7,69 @@ var id = '';
 var dependencies = ['./enclose'];
 
 function factory(enclose) {
-  var key = {};
+  var Example;
 
-  function Example(example) {
-    enclose(this, key);
-    this.setExample(example);
-    this.example = 'This is not the example you’re looking for.';
+  (function () {
+    var key = {};
+
+    Example = function Example(example) {
+      enclose(this, key);
+      this.setExample(example);
+      this.example = 'This is not the example you’re looking for.';
+    };
+
+    Example.prototype.getExample = function getExample() {
+      return this.$(key).example;
+    };
+
+    Example.prototype.setExample = function setExample(example) {
+      this.$(key).example = example;
+    };
+
+  }());
+
+  var okay = true;
+  var x = new Example('Hello, Example!');
+  if (x.getExample() === 'Hello, Example!') {
+    console.log('pass: Hello, Example!');
+  } else {
+    console.log('FAIL: Hello, Example!');
+    okay = false;
   }
 
-  Example.prototype.getExample = function getExample() {
-    return this.$(key).example;
-  };
-
-  Example.prototype.setExample = function setExample(example) {
-    this.$(key).example = example;
-  };
-
-  var x = new Example('Hello, Example!');
-  console.log(x.getExample());
-
   x.setExample('Goodbye, Example!');
-  console.log(x.getExample());
+  if (x.getExample() === 'Goodbye, Example!') {
+    console.log('pass: Goodbye, Example!');
+  } else {
+    console.log('FAIL: Goodbye, Example!');
+    okay = false;
+  }
 
-  console.log(x);
+  if (x.example !== 'Goodbye, Example!') {
+    console.log('pass: This is not the example you’re looking for.');
+  } else {
+    console.log('FAIL: This is not the example you’re looking for.');
+    okay = false;
+  }
+
+  if (typeof key === 'undefined' && x.$({}).example === undefined) {
+    console.log('pass: Cannot access `key` or property-value store.');
+  } else {
+    console.log('FAIL: Cannot access `key` or property-value store.');
+    okay = false;
+  }
+
+  try {
+    console.log(delete x.$);
+    console.log('Maybe your runtime does not support property descriptors.');
+  } catch (e) {
+    console.log("'" + e.message + "'.");
+    console.log('Your runtime supports property descriptors.');
+  }
+
+  if (!okay && process && typeof process.exit === 'function') {
+    process.exit(1);
+  }
 }
 
 // -----------------------------------------------------------------------------
